7647e21a07718f91c05593fd8c9cfffa1fc4669a
Mubelotix <mubelotix@gmail.com>
2024-10-04 10:16:19 +0200

    Update idndef

--- include/filesystem.h
+++ include/filesystem.h
@@ -1,7 +1,6 @@
-#ifndef TEST_H
-#define TEST_H
+#ifndef FILESYSTEM_H
+#define FILESYSTEM_H
 
-const char *get_path(const char *request);
 const char* get_content_type(const char *path);
 int send_file(int socket, const char *path);
 void send_directory_listing(int socket, const char *path);
